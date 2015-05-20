bb356ee5e204f9d69ba50ca3c0f136f30aecef79
Boris Nagaev <bnagaev@gmail.com>
2015-05-20 22:08:46 +0300

    исправил название модуля в тестах

--- c-distance/spec.lua
+++ c-distance/spec.lua
@@ -1,23 +1,23 @@
 describe("distance (implementation)", function()
     pending("loads module", function()
-        local f = require 'c-distance'
+        local f = require 'distance'
     end)
 
     pending("returns equally distant string", function()
-        local f = require 'c-distance'
+        local f = require 'distance'
         local middle = f('00', '11')
         assert(middle == '01' or middle == '10')
     end)
 
     pending("returns nil if no equally distant string exists",
     function()
-        local f = require 'c-distance'
+        local f = require 'distance'
         assert(f('0', '1') == nil)
     end)
 
     pending("throws error if length if input strings differ",
     function()
-        local f = require 'c-distance'
+        local f = require 'distance'
         assert.has_error(function()
             f('0', '11')
         end)
@@ -25,7 +25,7 @@ describe("distance (implementation)", function()
 
     pending("throws error if an argument is not string",
     function()
-        local f = require 'c-distance'
+        local f = require 'distance'
         assert.has_error(function()
             f('0', {})
         end)
@@ -36,7 +36,7 @@ describe("distance (implementation)", function()
 
     pending("throws error if input contains non-0-or-1",
     function()
-        local f = require 'c-distance'
+        local f = require 'distance'
         assert.has_error(function()
             f('00', 'aa')
         end)
