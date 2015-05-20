39b8dbe4a6cd0f36678f24498c3a8e293d675057
Boris Nagaev <bnagaev@gmail.com>
2015-05-20 19:29:03 +0300

    distance: another solution

--- lua-distance/spec.lua
+++ lua-distance/spec.lua
@@ -36,6 +36,40 @@ describe("distance (unit test)", function()
         end)
     end)
 
+    pending("accepts true solution 2", function()
+        local f = require 'distance.credit'
+        assert.has_no_error(function()
+            f(function(A, B)
+                assert(type(A) == 'string')
+                assert(type(B) == 'string')
+                assert(#A == #B)
+                local result = {}
+                local mismatches = 0
+                for i = 1, #A do
+                    local a = A:sub(i, i)
+                    local b = B:sub(i, i)
+                    assert(a == '0' or a == '1')
+                    assert(b == '0' or b == '1')
+                    if a == b then
+                        local c = (a == '0') and '1' or '0'
+                        table.insert(result, c)
+                    else
+                        if mismatches % 2 == 0 then
+                            table.insert(result, a)
+                        else
+                            table.insert(result, b)
+                        end
+                        mismatches = mismatches + 1
+                    end
+                end
+                if mismatches % 2 == 1 then
+                    return nil
+                end
+                return table.concat(result)
+            end)
+        end)
+    end)
+
     pending("throws on bad solutions", function()
         local f = require 'distance.credit'
         assert.has_error(function()
