ef50b8c80b8c5e6c00f8972e835376acf204adcd
dmitrypenzar1996 <dmitry_penzar_1996@mail.ru>
2015-05-23 14:12:16 +0300

    Update credit.lua

--- lua-shuf/credit.lua
+++ lua-shuf/credit.lua
@@ -1,5 +1,27 @@
 local function g(f)
-    -- implement it here
+	local n = 1000
+	local occurense = {}
+	for i = 1, n do
+		n[i] = 0
+	end
+	local result = f(n)
+
+	assert(type(result) == "table")
+	assert(#result == n)
+	for i = 1, n do
+		occurense[result[i]] = 1
+	end
+	
+	local sum = 0
+	for i = 1, n do
+		sum = sum + occurense[i]
+	end
+	assert(sum == n)
+	assert(f(0) == nil)
+	
+	assert(pcall(f('hjhg')))
+	assert(pcall(f('1')))
+	assert(pcall(f(1)))
 end
 
 return g
