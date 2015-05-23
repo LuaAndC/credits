4ab015c4734ecb8b1e527409b02f07d5f5c09828
dmitrypenzar1996 <dmitry_penzar_1996@mail.ru>
2015-05-23 15:58:22 +0300

    Solution shulf

--- lua-shuf/credit.lua
+++ lua-shuf/credit.lua
@@ -1,5 +1,34 @@
 local function g(f)
-    -- implement it here
+	local n = 1000
+	local occurense = {}
+	for i = 1, n do
+		n[i] = 0
+	end
+	local result = f(n)
+
+	assert(type(result) == "table")
+	assert(#result == n)
+	for i = 1, n do
+		occurense[result[i]] = 1
+	end
+	
+	local sum = 0
+	for i = 1, n do
+		sum = sum + occurense[i]
+	end
+	assert(sum == n)
+
+	local len = 0
+	for k, y in ipairs(result) do
+		len = len + 1
+	end
+
+	assert(len == n) 
+	assert(f(0) == {})
+	
+	assert(pcall(f('hjhg')))
+	assert(pcall(f('1')))
+	assert(pcall(f(1)))
 end
 
 return g
